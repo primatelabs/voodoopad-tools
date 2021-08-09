2d41216dda9c0f7d0756d2e67e7153cbc1f060a8
pl-ben <ben.richard@primatelabs.com>
2021-08-09 17:31:45 -0400

    Added password support to the voodoopad_import.py script

--- scripts/voodoopad_import.py
+++ scripts/voodoopad_import.py
@@ -4,12 +4,17 @@ import sys
 
 def main():
 
-  if len(sys.argv) != 3:
-    print('usage: voodoopad_import.py <document> <directory>')
+  if len(sys.argv) != 3 and len(sys.argv) != 4:
+    print('usage: voodoopad_import.py <document> <directory> <password>')
     return
 
   document = sys.argv[1]
   directory = sys.argv[2]
+
+  if len(sys.argv) == 4:
+    password = sys.argv[3]
+  else:
+    password = None
 
   files = os.listdir(directory)
 
@@ -17,8 +22,11 @@ def main():
     article_name = os.path.splitext(f)[0]
     file_path = directory + '/' + f
 
+    if password != None:
+      cmdline = 'python3 voodoopad.py {0} add {1} {2} markdown --password {3}'.format(document, file_path, article_name, password)
+    else:
+      cmdline = 'python3 voodoopad.py {0} add {1} {2} markdown'.format(document, file_path, article_name)
 
-    cmdline = 'python3 voodoopad.py {0} add {1} {2} markdown'.format(document, file_path, article_name)
     print(cmdline)
     os.system(cmdline)
 
